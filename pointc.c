910bd33a80c4b459c47013c64c1766f78a2103c3
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-18 09:10:42 -0400

    Update pointc.c

--- pointc.c
+++ pointc.c
@@ -3,22 +3,22 @@ int main(){
    int* pc, c;
    
    c = 66;
-   printf("Address of c: %p\n", &c);
+  // printf("Address of c: %p\n", &c);
    
    printf("Value of c: %d\n\n", c);  // 66
    
    pc = &c;
-   printf("Address of pointer pc: %p\n", pc);
+  // printf("Address of pointer pc: %p\n", pc);
    
    printf("Content of pointer pc: %d\n\n", *pc); // 66
    
    c = 15;
-   printf("Address of pointer pc: %p\n", pc);
+//   printf("Address of pointer pc: %p\n", pc);
    
    printf("Content of pointer pc: %d\n\n", *pc); // 15
    
    *pc = 3;
-   printf("Address of c: %p\n", &c);
+ //  printf("Address of c: %p\n", &c);
    
    printf("Value of c: %d\n\n", c); // 3
    return 0;
